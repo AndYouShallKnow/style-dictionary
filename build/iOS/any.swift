16acb3fc64b1784ad965d6e613ede36c67c59483
AndYouShallKnow <AndYouShallKnow@users.noreply.github.com>
2024-03-28 11:59:44 +0000

    Token Conversaion

--- build/iOS/any.swift
+++ build/iOS/any.swift
@@ -4,11 +4,15 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 28 Mar 2024 11:38:48 GMT
+// Generated on Thu, 28 Mar 2024 11:59:44 GMT
 
 
 import UIKit
 
 public class  {
-    
+    public static let dark = #111111
+    public static let green = #00FF00
+    public static let light = #CCCCCC
+    public static let medium = #999999
+    public static let red = #FF0000
 }
