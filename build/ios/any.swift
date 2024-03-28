7d089e812c238d12adf025c6eb28e5891d03655e
AndYouShallKnow <AndYouShallKnow@users.noreply.github.com>
2024-03-28 10:35:29 +0000

    Update tokens from Figma

---
+++ build/ios/any.swift
@@ -0,0 +1,14 @@
+
+//
+// any.swift
+//
+
+// Do not edit directly
+// Generated on Thu, 28 Mar 2024 10:35:29 GMT
+
+
+import UIKit
+
+public class  {
+    
+}
---
+++ build/ios/enum.swift
@@ -0,0 +1,14 @@
+
+//
+// enum.swift
+//
+
+// Do not edit directly
+// Generated on Thu, 28 Mar 2024 10:35:29 GMT
+
+
+import UIKit
+
+public enum  {
+    
+}
