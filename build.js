dc0ede61eb0df370c800818a962038e91c00ab2d
Gerard <131524570+AndYouShallKnow@users.noreply.github.com>
2024-04-02 20:14:27 +0100

    Update build.js

--- build.js
+++ build.js
@@ -1,25 +1,20 @@
 
 //Style Dictionary Transforms for Tokens Studio bit, not working
 
-//CJS
-// const { registerTransforms } = require('@tokens-studio/sd-transforms');
-// const StyleDictionary = require('style-dictionary');
+import { registerTransforms } from "@tokens-studio/sd-transforms";
+import StyleDictionary from "style-dictionary";
 
-// ES6?
-// import { registerTransforms } from "@tokens-studio/sd-transforms";
-// import StyleDictionary from "style-dictionary";
-
-// If this line works, the 'tokens-studio' lib will be available to be
-// registerTransforms(StyleDictionary);
+If this line works, the 'tokens-studio' lib will be available to be
+registerTransforms(StyleDictionary);
 
 // StyleDictionary bit
 const sd = require('style-dictionary').extend({
   source: ['tokens/*.json'],
   platforms: {
     scss: {
-      transformGroup: 'scss',
+      //transformGroup: 'scss',
       // This transform group isn't working due to the above init not working
-      // transformGroup: 'tokens-studio',
+      transformGroup: 'tokens-studio',
       buildPath: 'build/',
       files: [{
         destination: 'variables.scss',
