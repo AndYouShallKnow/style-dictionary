05817bffe471f0e896aba7a829817b58f8933a44
Gerard <131524570+AndYouShallKnow@users.noreply.github.com>
2024-04-02 11:45:56 +0100

    Update build.js

--- build.js
+++ build.js
@@ -1,3 +1,13 @@
+import { registerTransforms } from "@tokens-studio/sd-transforms";
+
+import StyleDictionary from "style-dictionary";
+
+
+
+// will register them on StyleDictionary object
+// that is installed as a dependency of this package.
+registerTransforms(StyleDictionary);
+
 const StyleDictionary = require('style-dictionary').extend({
   source: ['output/**/*.json'],
   platforms: {
