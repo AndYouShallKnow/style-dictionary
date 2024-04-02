baedffc53e1dc581b9505fc6ca7f583b3a2e9c1c
Gerard <131524570+AndYouShallKnow@users.noreply.github.com>
2024-04-02 12:56:05 +0100

    Update build.js

--- build.js
+++ build.js
@@ -1,47 +1,19 @@
-const { registerTransforms } = require('@tokens-studio/sd-transforms');
-const StyleDictionary = require('style-dictionary');
-
-// will register them on StyleDictionary object
-// that is installed as a dependency of this package.
-registerTransforms(StyleDictionary);
 
 
-const sd = StyleDictionary.extend({
+
+const StyleDictionary = require('style-dictionary').extend({
   source: ['output/**/*.json'],
   platforms: {
-    css: {
+    scss: {
       transformGroup: 'tokens-studio',
-      transforms: ['name/kebab'], // <-- add a token name transform for generating token names, default is camel
-      buildPath: 'build/css/',
-      files: [
-        {
-          destination: 'variables.css',
-          format: 'css/variables',
-        },
-      ],
-    },
-  },
+      buildPath: 'build/',
+      files: [{
+        destination: 'variables.scss',
+        format: 'scss/variables'
+      }]
+    }
+    // ...
+  }
 });
 
-sd.cleanAllPlatforms();
-sd.buildAllPlatforms();
-
-
-
-
-// const StyleDictionary = require('style-dictionary').extend({
-//   source: ['output/**/*.json'],
-//   platforms: {
-//     scss: {
-//       transformGroup: 'tokens-studio',
-//       buildPath: 'build/',
-//       files: [{
-//         destination: 'variables.scss',
-//         format: 'scss/variables'
-//       }]
-//     }
-//     // ...
-//   }
-// });
-
-// StyleDictionary.buildAllPlatforms();
+StyleDictionary.buildAllPlatforms();
