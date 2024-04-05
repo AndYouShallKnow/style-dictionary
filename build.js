26e5d2cfc264832a3f068c25c01e93c1d20bc3ab
Gerard <gerardwhite@icloud.com>
2024-04-05 14:13:21 +0100

    working via node

--- build.js
+++ build.js
@@ -1,24 +1,19 @@
 
-//Style Dictionary Transforms for Tokens Studio bit
-const { registerTransforms } = require('@tokens-studio/sd-transforms');
-const StyleDictionary = require('style-dictionary');
-registerTransforms(StyleDictionary);
 
 const sd = require('style-dictionary').extend({
    source: ['tokens/*.json'],
-  //source: ['https://github.com/gerard-figma/variables-github-action-example/blob/main/colors.json'],
   platforms: {
     scss: {
-      transformGroup: 'tokens-studio',
-      buildPath: 'build/',
+      transformGroup: 'scss',
+      buildPath: 'build/javascript/',
       files: [{
           destination: 'variables.js',
           format: 'javascript/es6'
       }]
     },
       ios: {
-      buildPath: 'build/',
-      transformGroup: 'tokens-studio',
+      buildPath: 'build/ios/',
+      transformGroup: 'ios-swift',
       prefix: 'sd',
       files: [
         {
--- variables.js
+++
@@ -1,6 +0,0 @@
-/**
- * Do not edit directly
- * Generated on Wed, 03 Apr 2024 12:31:45 GMT
- */
-
-
