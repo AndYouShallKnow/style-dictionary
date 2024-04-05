ddd79e521168d944d608434c463dff70f163a952
Gerard <gerardwhite@icloud.com>
2024-04-05 14:42:38 +0100

    switch from js

--- build.js
+++ build.js
@@ -1,31 +1,31 @@
 
 
-const sd = require('style-dictionary').extend({
-   source: ['tokens/*.json'],
-  platforms: {
-    scss: {
-      transformGroup: 'scss',
-      buildPath: 'build/javascript/',
-      files: [{
-          destination: 'variables.js',
-          format: 'javascript/es6'
-      }]
-    },
-      ios: {
-      buildPath: 'build/ios/',
-      transformGroup: 'ios-swift',
-      prefix: 'sd',
-      files: [
-        {
-          destination: 'any.swift',
-          format: 'ios-swift/any.swift'
-        }
-      ]
-    }
-    // ...
-  }
-});
+// const sd = require('style-dictionary').extend({
+//    source: ['tokens/*.json'],
+//   platforms: {
+//     scss: {
+//       transformGroup: 'scss',
+//       buildPath: 'build/javascript/',
+//       files: [{
+//           destination: 'variables.js',
+//           format: 'javascript/es6'
+//       }]
+//     },
+//       ios: {
+//       buildPath: 'build/ios/',
+//       transformGroup: 'ios-swift',
+//       prefix: 'sd',
+//       files: [
+//         {
+//           destination: 'any.swift',
+//           format: 'ios-swift/any.swift'
+//         }
+//       ]
+//     }
+//     // ...
+//   }
+// });
 
 
-// sd.cleanAllPlatforms();
-sd.buildAllPlatforms();
+// // sd.cleanAllPlatforms();
+// sd.buildAllPlatforms();
